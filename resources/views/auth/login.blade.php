880c95194238bed52adb34dfae1b046e86b290e5
jareee <fajarmulyos@gmail.com>
2022-09-25 09:19:03 +0700

    add auth

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="signin.css">
+    <title>Peminjaman Aula | login form</title>
+</head>
+<body>
+    <div class="bg">
+    <div class="form">
+        <img src="img/hall.png" class="image" >
+        <h2>SIGN IN</h2>
+        <form action="{{ route('login') }}" method="post" class="input-group">
+            @csrf
+            <div>
+            <input class="input @error('email') is-error @enderror" name="email" type="email" placeholder="Email">
+            @error('email')
+            <label for="">{{ $message }}</label>
+            @enderror
+            </div>
+            <div>
+            <input class="input @error('password') is-error @enderror" name="password" type="password" placeholder="Password">
+            @error('password')
+            <label for="">{{ $message }}</label>
+            @enderror
+            </div>
+            <div>
+            <input class="submit" type="submit" value="SIGN IN">
+            </div>
+            <small class="acc" >Don't have an account ? <a href="{{ route('register') }}">Register</a></small>
+        </form>
+    </div>
+</div>
+</body>
+</html>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="signup.css">
+    <title>Peminjaman Aula | register form</title>
+</head>
+<body>
+    <div class="bg">
+    <div class="form">
+        <img src="img/hall.png" class="image" >
+        <h2>SIGN UP</h2>
+        <form action="{{ route('register') }}" method="POST" class="input-group">
+        @csrf 
+            <div>
+                <input class="input @error('username') is-error @enderror"  name="username" id="username" type="text" placeholder="Username">
+                @error('username')
+                    <label for="">{{ $message }}</label>
+                @enderror
+            </div>
+
+            <div>
+                <input class="input @error('email') is-error @enderror" name="email" id="email" type="email" placeholder="Email">
+                @error('email')
+                    <label for="">{{ $message }}</label>
+                @enderror
+            </div>
+
+            <div>
+            <input class="input @error('password') is-error @enderror" name="password" type="password" placeholder="Password">
+            @error('password')
+                <label for="">{{ $message }}</label>
+            @enderror
+            </div>
+
+            <div>
+            <input class="input @error('password_confirmation') is-error @enderror" name="password_confirmation" type="password" placeholder="Confirm Password">
+            @error('password_confirmation')
+                <label for="">{{ $message }}</label>
+            @enderror
+            </div>
+
+            <div>
+            <input class="submit" type="submit" value="SIGN UP">
+            </div>
+            <small class="acc" >Already have an account ? <a href="{{ route('login') }}">Sign in</a></small>
+        </form>
+    </div>
+</div>
+</body>
+</html>

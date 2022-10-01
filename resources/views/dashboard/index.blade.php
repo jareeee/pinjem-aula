6afcd7070b16c1610dfb4617a5fa4789a2e5c7b9
jareee <fajarmulyos@gmail.com>
2022-10-01 21:26:56 +0700

    add dashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,25 @@
+@extends('dashboard.layout.layout')
+
+@section('content')
+      <div class="d-flex justify-content-around flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Tentang kamu</h1>
+        <h1 class="h2">Jadwal saya</h1>
+      </div>
+
+      <div class="container">
+        <div class="profil">
+          <h4>Nama lengkap</h4>
+        </div>
+        <div class="jadwal">
+          <div class="jadwal-box">
+            
+          </div>
+        </div>
+      </div>
+
+  </div>
+</div>
+
+
+
+@endsection
